06ae990afd5ebe377c122e3fd194b66279125acc
Rohov <rogov.dmitry@gmail.com>
2018-10-18 17:21:00 +0300

    ➕ disable port suggestion after manul input

--- src/features/SubmitForm.js
+++ src/features/SubmitForm.js
@@ -13,67 +13,88 @@ export const SubmitForm = compose(
   // @material-ui/core forces to set initial state so animation works properly
   withState("port", "setPort", ""),
   withState("coin", "setCoin"),
+  withState("isPortIsManuallyEdited", "setIsPortIsManuallyEdited", false),
   setDisplayName("SubmitForm")
-)(({ ip, port, coin, setIP, setPort, setCoin, onSubmit }) => (
-  <>
-    <br />
-    <Grid container spacing={16}>
-      <Grid item xs={12}>
-        <TextFieldEditable
-          autoFocus={false}
-          label={"IP"}
-          fullWidth
-          variant={"outlined"}
-          onChange={e => setIP(e.target.value)}
-          error={!!ip && !isValidIP(ip)}
-          helperText={!!ip && !isValidIP(ip) ? "IP is not valid" : undefined}
-        />
+)(
+  ({
+    ip,
+    port,
+    coin,
+    setIP,
+    setPort,
+    setCoin,
+    onSubmit,
+    isPortIsManuallyEdited,
+    setIsPortIsManuallyEdited
+  }) => (
+    <>
+      <br />
+      <Grid container spacing={16}>
+        <Grid item xs={12}>
+          <TextFieldEditable
+            autoFocus={false}
+            label={"IP"}
+            fullWidth
+            variant={"outlined"}
+            onChange={e => setIP(e.target.value)}
+            error={!!ip && !isValidIP(ip)}
+            helperText={!!ip && !isValidIP(ip) ? "IP is not valid" : undefined}
+          />
+        </Grid>
+        <Grid item xs={8} sm={9}>
+          <Suggestions onSelect={item => setCoin(item)} />
+        </Grid>
+        <Grid item xs={4} sm={3}>
+          <TextFieldEditable
+            label={"Port"}
+            fullWidth
+            variant={"outlined"}
+            onChange={e => {
+              setPort(e.target.value);
+              setIsPortIsManuallyEdited(true);
+            }}
+            type={"number"}
+            error={!!port && !isValidPort(port)}
+            helperText={
+              <>
+                {!!port && !isValidPort(port) ? (
+                  "Port range is 1 - 65535"
+                ) : !!coin &&
+                getPort(coin) &&
+                parseInt(port, 10) !== parseInt(getPort(coin), 10) ? (
+                  <Typography variant={"caption"} gutterBottom>
+                    Suggestion:
+                    <u
+                      style={{
+                        marginLeft: "4px",
+                        cursor: "pointer"
+                      }}
+                      onClick={() => setPort(getPort(coin))}
+                    >
+                      {getPort(coin)}
+                    </u>
+                  </Typography>
+                ) : null}
+              </>
+            }
+            InputProps={{
+              value:
+                !isPortIsManuallyEdited && coin && !!getPort(coin)
+                  ? getPort(coin)
+                  : port
+            }}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={8} sm={9}>
-        <Suggestions onSelect={item => setCoin(item)} />
-      </Grid>
-      <Grid item xs={4} sm={3}>
-        <TextFieldEditable
-          label={"Port"}
-          fullWidth
-          variant={"outlined"}
-          onChange={e => setPort(e.target.value)}
-          type={"number"}
-          error={!!port && !isValidPort(port)}
-          helperText={
-            <>
-              {!!port && !isValidPort(port) ? (
-                "Port range is 1 - 65535"
-              ) : !!coin && getPort(coin) && port !== getPort(coin) ? (
-                <Typography variant={"caption"} gutterBottom>
-                  Suggestion:
-                  <u
-                    style={{
-                      marginLeft: "4px",
-                      cursor: "pointer"
-                    }}
-                    onClick={() => setPort(getPort(coin))}
-                  >
-                    {getPort(coin)}
-                  </u>
-                </Typography>
-              ) : null}
-            </>
-          }
-          InputProps={{
-            value: port
-          }}
-        />
-      </Grid>
-    </Grid>
-    <br />
-    <Button
-      color={"primary"}
-      variant={"contained"}
-      onClick={() => onSubmit({ ip, port, coin })}
-      disabled={!port || !coin || !ip || !isValidIP(ip) || !isValidPort(port)}
-    >
-      Check
-    </Button>
-  </>
-));
+      <br />
+      <Button
+        color={"primary"}
+        variant={"contained"}
+        onClick={() => onSubmit({ ip, port, coin })}
+        disabled={!port || !coin || !ip || !isValidIP(ip) || !isValidPort(port)}
+      >
+        Check
+      </Button>
+    </>
+  )
+);
