2d2ec864fbcd37448adfce9902097489da3ba93d
Rohov <rogov.dmitry@gmail.com>
2018-10-19 12:28:33 +0300

    ➕ generic error boundry handler for ws subscription

--- src/features/SubmitFormWithUnits.js
+++ src/features/SubmitFormWithUnits.js
@@ -37,14 +37,45 @@ const ADD_URL = gql`
   }
 `;
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    console.log(error);
+    console.log(info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
 const SubscriptionForNewUnits = ({ variables, onSubscriptionData }) => (
-  <Subscription
-    subscription={UNITS_SUBSCRIPTION}
-    variables={variables}
-    onSubscriptionData={onSubscriptionData}
-  >
-    {({ loading, error, data }) => null}
-  </Subscription>
+  <ErrorBoundary>
+    <Subscription
+      subscription={UNITS_SUBSCRIPTION}
+      variables={variables}
+      onSubscriptionData={onSubscriptionData}
+    >
+      {({ loading, error, data }) => {
+        if (error) {
+          console.log(error);
+          return "Error";
+        }
+        return null;
+      }}
+    </Subscription>
+  </ErrorBoundary>
 );
 
 export const SubmitFormWithUnits = compose(
