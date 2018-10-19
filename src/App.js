64d823fa154e9fbdcf1723442ea05bfa54a55c44
Rohov <rogov.dmitry@gmail.com>
2018-10-19 18:06:59 +0300

    📖 info panel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { CssBaseline, Grid, MuiThemeProvider } from "@material-ui/core";
+import {
+  CssBaseline,
+  Grid,
+  MuiThemeProvider,
+  Paper,
+  Typography
+} from "@material-ui/core";
 import React, { Component } from "react";
 
 import { ApolloProvider } from "react-apollo";
@@ -14,17 +20,54 @@ class App extends Component {
         <MuiThemeProvider theme={theme}>
           <CssBaseline />
           <Wallpaper />
-          <Grid container justify={"center"} style={{ padding: "12px" }}>
-            <Grid item xs={12} sm={8} md={6} lg={6} xl={4}>
-              <div style={{ marginTop: "-80px" }}>
-                <SubmitFormWithUnits />
-              </div>
-              <br />
-              <br />
-              <br />
-              <br />
+          <div style={{ padding: "12px" }}>
+            <Grid container spacing={24}>
+              <Grid item xs={12} sm={2} md={3} xl={4}>
+                <Paper style={{ padding: "20px" }}>
+                  <Typography variant={"h6"} gutterBottom>
+                    What’s this?
+                  </Typography>
+                  <Typography variant={"body2"} paragraph>
+                    This is a tool for instant check of a masternode health.
+                  </Typography>
+                </Paper>
+                <br />
+                <Paper style={{ padding: "20px" }}>
+                  <Typography variant={"h6"} gutterBottom>
+                    How can it helps you?
+                  </Typography>
+                  <Typography variant={"body2"} paragraph>
+                    It gives you information about wellness of your masternode.
+                    <ul>
+                      <li>does it answering properly </li>
+                      <li>does it operating in a right network</li>
+                      <li>what are other nodes thinking about your one</li>
+                      <li>...and a lot of other useful information</li>
+                    </ul>
+                  </Typography>
+                </Paper>
+                <br />
+                <Paper style={{ padding: "20px" }}>
+                  <Typography variant={"h6"} gutterBottom>
+                    How it differ from others?
+                  </Typography>
+                  <Typography variant={"body2"} paragraph>
+                    This is the only service that connects to your masternode
+                    directly and does a most of checks in real time.
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={8} md={6} xl={4}>
+                <div style={{ marginTop: "-160px" }}>
+                  <SubmitFormWithUnits />
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+              </Grid>
             </Grid>
-          </Grid>
+          </div>
         </MuiThemeProvider>
       </ApolloProvider>
     );
