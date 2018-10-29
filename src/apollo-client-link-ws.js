f7bff6aa92058418105a6224bb873d79ac702063
Rohov <rogov.dmitry@gmail.com>
2018-10-29 16:55:57 +0200

    ➕ lazy ws connection

--- src/apollo-client-link-ws.js
+++ src/apollo-client-link-ws.js
@@ -4,11 +4,12 @@ export default new WebSocketLink({
   uri:
     process.env.NODE_ENV === "production"
       ? "ws://checkmynode.com/subscriptions"
-      : `ws://127.0.0.1:8080/subscriptions`,
+      : `ws://checkmynode.com/subscriptions`,
   options: {
     reconnect: true,
-    lazy: false,
+    lazy: true,
     connectionCallback: error => {
+      console.log("Connection callback");
       if (!!error) console.log(error);
     }
   }
