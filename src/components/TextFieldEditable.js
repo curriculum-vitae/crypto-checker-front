3e5113aa354ac98f2d8cb8c7184dd752a2a379d4
Rohov <rogov.dmitry@gmail.com>
2018-11-01 13:30:57 +0200

    ➖

--- src/components/TextFieldEditable.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
-import { flow } from "lodash/fp";
-import { withState } from "recompose";
-
-export const TextFieldEditable = flow(
-  withState("value", "setValue", ({ value }) => value)
-)(({ ...props }) => <TextField {...props} />);
