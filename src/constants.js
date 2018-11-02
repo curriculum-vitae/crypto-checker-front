c1d3ae31a1aaeeb6f3b33a6da4e199daf7da5939
Stealth <eventqueue@gmail.com>
2018-11-02 20:08:50 +0300

    Use external config

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,10 @@
-import COINS_LOCAL from "coins.json";
-
 export const CONFIG_KEY = "REACT_APP_GLOBAL_NODE_CHECKER_CONFIG_$";
 
 const CONFIG = window[CONFIG_KEY] || {};
 
 export const URI_LINK_WEBSOCKET =
-  CONFIG["URI_LINK_WEBSOCKET"] || process.env.NODE_ENV === "production"
-    ? "wss://checkmynode.com/subscriptions"
-    : `wss://checkmynode.com/subscriptions`;
+  CONFIG["URI_LINK_WEBSOCKET"] || "wss://checkmynode.com/subscriptions";
 
-export const URL_LINK_HTTP =
-  CONFIG["URL_LINK_HTTP"] || process.env.NODE_ENV === "production"
-    ? "/graphql"
-    : "http://127.0.0.1:8080/graphql";
+export const URL_LINK_HTTP = CONFIG["URL_LINK_HTTP"] || "/graphql";
 
-export const COINS = CONFIG_KEY["coins"] || COINS_LOCAL;
+export const COINS = CONFIG["COINS"] || [["DASH", "Dash", 9999]];
