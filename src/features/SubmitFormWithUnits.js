2148de91c484b0976064291bba1ca5b26f820ef6
Rohov <rogov.dmitry@gmail.com>
2018-11-05 14:05:06 +0200

    🔄

--- src/features/SubmitFormWithUnits.js
+++ src/features/SubmitFormWithUnits.js
@@ -156,7 +156,7 @@ export const SubmitFormWithUnits = compose(
                 align={"right"}
                 style={{ color: blue[700] }}
               >
-                Permalink link
+                Direct link to this check
               </Typography>
             </a>
             <Subscription
